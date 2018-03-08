0d3519ac925fde901392235a2e25db0f37fa1d46
mvishal <mvishu405@gmail.com>
2018-03-08 18:00:36 +0530

    route updated

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -12,10 +12,10 @@ Route::middleware('web')->group(function () {
 
     Route::auth();
 
-    Route::get(Adminlte::getAdminUrl('/'), 'AdminController\DashboardController@index')->name('admin');
+    /*Route::get(Adminlte::getAdminUrl('/'), 'AdminController\DashboardController@index')->name('admin');
 
     Route::get(Adminlte::getAdminUrl('setting'), 'AdminController\SettingController@showSetting')->name('admin.setting');
 
-    Route::post(Adminlte::getAdminUrl('storeSetting'), 'AdminController\SettingController@storeSetting')->name('admin.store.setting');
+    Route::post(Adminlte::getAdminUrl('storeSetting'), 'AdminController\SettingController@storeSetting')->name('admin.store.setting');*/
 
 });
